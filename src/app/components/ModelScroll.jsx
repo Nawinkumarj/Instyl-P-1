7d0ded4fe9af4d46384cb03a54bd4555ab20ca75
Praveen Kumar <pk8497121@gmail.com>
2025-09-07 13:49:40 +0530

    mobile

--- src/app/components/ModelScroll.jsx
+++ src/app/components/ModelScroll.jsx
@@ -72,7 +72,7 @@ export default function ModelScroll() {
 
       useGSAP(
         () => {
-           if (window.innerWidth <= 568) return;
+          //  if (window.innerWidth <= 568) return;
           const para = paraRef.current;
           const heading = headingRef.current;
           if (!para || !heading) return;
