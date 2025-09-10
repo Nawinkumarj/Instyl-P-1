7d034de2d796c7ca62cc62692996bd339eca58bd
Nawinkumar <nawinkumar@Nawins-MacBook-Pro.local>
2025-09-10 13:00:27 +0530

    NK

--- src/app/Privacy/page.jsx
+++ src/app/Privacy/page.jsx
@@ -1,10 +1,9 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function page() {
   return (
-    <View>
-      <Text>page</Text>
-    </View>
+    <div>
+      Privacy
+    </div>
   )
 }
